9110639c8791446d0e97b66c7341774fd425b702
olesia <Trapeznikova1803@gmail.com>
2023-02-08 01:30:38 +0300

    step 4

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,8 @@
 import readlineSync from 'readline-sync';
 
 const greeting = () => {
-let userName = readlineSync.question('May I have your name?');
-console.log('Welcome to the Brain Games!');
-console.log(`Hello, ${userName}!`);
+  const userName = readlineSync.question('May I have your name?');
+  console.log('Welcome to the Brain Games!');
+  console.log(`Hello, ${userName}!`);
 };
 export default greeting;
